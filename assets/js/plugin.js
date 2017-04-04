6a9362b3310a45c6e946ff12fe87f3f4e28e17a0
Matt Watson <matt@makedo.net>
2017-04-04 15:27:55 +0100

    Updated Styles

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,6 +1,9 @@
 ( function( $ ) {
 	'use strict';
 
+	/**
+	 * Remove alert when x clicked.
+	 */
 	$( '.mkdo_alert .close' ).click( function(){
 		$(this).closest( '.mkdo_alert' ).fadeOut( function() {
 			$(this).slideUp( function() {
